1e7faeead3f48e699d00f213c5a7a857769ab52c
Manuel Valles <manukempo@gmail.com>
2020-09-17 08:42:03 +0100

    Fixed Collision Detection - React.StrictMode not working

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -19,7 +19,7 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   const movePlayer = (dir) => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
