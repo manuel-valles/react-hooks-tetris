946f6e87b7ada8efb000ad5c39de8c7ecab50262
manukempo <manukempo@gmail.com>
2020-09-17 16:37:01 +0000

    Updates

---
+++ precache-manifest.e2775e8aca922701b52c2e78f34e3249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632b882a4c66e3818de6a0b628868245",
+    "url": "/react-hooks-tetris/index.html"
+  },
+  {
+    "revision": "d4259e578264e4a1f06e",
+    "url": "/react-hooks-tetris/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bcf8cd8327015a8701ee",
+    "url": "/react-hooks-tetris/static/js/2.a46f122a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hooks-tetris/static/js/2.a46f122a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4259e578264e4a1f06e",
+    "url": "/react-hooks-tetris/static/js/main.56e9780c.chunk.js"
+  },
+  {
+    "revision": "1b3647d28bd417478f2b",
+    "url": "/react-hooks-tetris/static/js/runtime-main.151af3f8.js"
+  },
+  {
+    "revision": "2bb7a3d7afb4b92785da54e363dd0204",
+    "url": "/react-hooks-tetris/static/media/background.2bb7a3d7.jpg"
+  }
+]);
